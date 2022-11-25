99d8e060675eb710032e40b1911e0c156b4f5518
raaaz <razijam996@gmail.com>
2022-11-25 19:31:28 +0330

    delete user

--- handelers/get.go
+++ handelers/get.go
@@ -2,9 +2,10 @@ package handelers
 
 import (
 	"Arc/model"
-	"net/http"
-	"github.com/labstack/echo"
 	"Arc/repository"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func GetAllUsers(c echo.Context) error {
@@ -14,14 +15,19 @@ func GetAllUsers(c echo.Context) error {
 	return c.JSON(http.StatusOK, users)
 }
 
-
 func GetUser(c echo.Context) error {
 	var user model.User
 	// User ID from path `users/:id`
- 	id := c.Param("id")
+	id := c.Param("id")
 	repository.Db.Where("id = ?", id).Find(&user)
- 	return c.JSON(http.StatusOK, user)
+	return c.JSON(http.StatusOK, user)
 }
 
 // select name, id from users
 // select * from users where id = 3;
+
+func DeleteUser(c echo.Context) error {
+	id := c.Param("id")
+	repository.Db.Delete(&model.User{}, id)
+	return c.JSON(http.StatusOK, struct{ Msg string }{Msg: "felan"})
+}
--- server.go
+++ server.go
@@ -14,8 +14,12 @@ func main() {
 	e.POST("/signup", handelers.Signup)
 	e.POST("/login", handelers.Login)
 // get /user -> list of all users
-	e.GET("/user", handelers.GetAllUsers)
+	e.GET("/users", handelers.GetAllUsers)
 // get /user/{id} -> get that user
-	e.GET("/users/:id", handelers.GetUser) 
+	e.GET("/users/:id", handelers.GetUser)
+// put /user/{id} -> put that user
+	e.PUT("/users/:id", handelers.UpdateUser)
+// delete /user/{id} -> delete that user 
+	e.DELETE("/users/:id", handelers.DeleteUser)
 	e.Logger.Fatal(e.Start(":3000"))
 }
